8dbeafe0fe8a6cc824fa30d4735359f387c0cbae
Justin Inácio <j.trollkarl@gmail.com>
2018-04-23 17:03:23 +0200

    build
    - enabled experimental mode for kotlin android plugin
    - updated kotlin runtime from jre to jdk

--- app/src/nodagger/java/com/moducode/daggerexample/ui/activity/MainActivity.kt
+++ app/src/nodagger/java/com/moducode/daggerexample/ui/activity/MainActivity.kt
@@ -2,7 +2,6 @@ package com.moducode.daggerexample.ui.activity
 
 
 import android.support.v4.app.Fragment
-import com.moducode.daggerexample.SingleFragmentActivity
 
 class MainActivity : SingleFragmentActivity() {
 
