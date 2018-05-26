d9fd61b6691cae9fa077535cec0537122a6f92fa
Justin Inácio <j.trollkarl@gmail.com>
2018-05-26 20:41:34 +0200

    update
    AppComponent is now held at application level

--- app/src/dagger/java/com.moducode.daggerexample/Injector.kt
+++ app/src/dagger/java/com.moducode.daggerexample/Injector.kt
@@ -1,27 +1,30 @@
 package com.moducode.daggerexample
 
-import com.moducode.daggerexample.dagger.ContextModule
-import com.moducode.daggerexample.dagger.DaggerAppComponent
 import com.moducode.daggerexample.ui.fragment.EpisodeDetailFragment
 import com.moducode.daggerexample.ui.fragment.EpisodeListFragment
 import com.moducode.daggerexample.ui.fragment.contract.EpisodeDetailContract
 import com.moducode.daggerexample.ui.fragment.contract.EpisodeListContract
+import timber.log.Timber
 
-fun EpisodeDetailFragment.buildPresenter(): EpisodeDetailContract.Actions =
-        DaggerAppComponent
-                .builder()
-                .contextModule(ContextModule(activity?.applicationContext!!))
-                .build()
-                .buildPresenterComponent()
-                .buildEpisodeDetailPresenter()
+fun EpisodeDetailFragment.buildPresenter(): EpisodeDetailContract.Actions {
+    val presenter = App.get(activity!!)
+            .component
+            .buildPresenterComponent()
+            .buildEpisodeDetailPresenter()
+    Timber.d("$presenter")
+    return presenter
+}
 
 
-fun EpisodeListFragment.buildPresenter(): EpisodeListContract.Actions =
-        DaggerAppComponent
-                .builder()
-                .contextModule(ContextModule(activity?.applicationContext!!))
-                .build()
-                .buildPresenterComponent()
-                .buildEpisodeListPresenter()
+fun EpisodeListFragment.buildPresenter(): EpisodeListContract.Actions {
+    val presenter = App.get(activity!!)
+            .component
+            .buildPresenterComponent()
+            .buildEpisodeListPresenter()
+    Timber.d("$presenter")
+    return presenter
+
+}
+
 
 
