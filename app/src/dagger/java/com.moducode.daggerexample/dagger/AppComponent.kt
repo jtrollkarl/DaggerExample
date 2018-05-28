29acee3abd8b52eb079316209af857f6d5d8a717
Justin Inácio <j.trollkarl@gmail.com>
2018-05-28 12:51:11 +0200

    update
    - keeping component functions for pulling individual dependencies

--- app/src/dagger/java/com.moducode.daggerexample/dagger/AppComponent.kt
+++ app/src/dagger/java/com.moducode.daggerexample/dagger/AppComponent.kt
@@ -1,9 +1,12 @@
 package com.moducode.daggerexample.dagger
 
 
+import com.moducode.daggerexample.room.DbRepo
+import com.moducode.daggerexample.service.EpisodeService
 import com.moducode.daggerexample.ui.fragment.EpisodeDetailPresenter
 import com.moducode.daggerexample.ui.fragment.EpisodeListPresenter
 import dagger.Component
+import okhttp3.OkHttpClient
 import javax.inject.Singleton
 
 @Singleton
@@ -13,5 +16,12 @@ interface AppComponent {
     fun buildEpisodeDetailPresenter(): EpisodeDetailPresenter
 
     fun buildEpisodeListPresenter(): EpisodeListPresenter
+
+    //these 3 functions returning dependencies are only used for demonstration purposes
+    fun okHttpClient(): OkHttpClient
+
+    fun episodeService(): EpisodeService
+
+    fun dbRepo(): DbRepo
 
 }
